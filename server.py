0ee78d2b7b96e6374bd12db6665b2140b3c19bfb
StaceyCarter <stak1903@gmail.com>
2019-05-14 17:06:06 -0700

    added tests for current flask routes & added user registration form

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 # Creates an instance of a Flask object
@@ -15,7 +15,25 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Renders the homepage"""
-    return "<h1>Hello there</h1>"
+    return render_template("homepage.html")
+
+@app.route('/adduser')
+def add_user():
+    """Renders a form to add a user to the database"""
+
+    
+
+    return render_template("add_user.html")
+
+@app.route('/register', methods=["POST"])
+def register_user():
+    """Adds a user to the database"""
+
+    ### ADD TO DATABASE
+
+    flash("Welcome to the database!")
+
+    return redirect('/')
 
 if __name__ == "__main__":
     app.debug = True
---
+++ tests.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from server import app
+
+class FlaskTests(TestCase):
+
+
+
+    def setUp(self):
+        """Completed before each test"""
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_index(self):
+        """Test the homepage is rendering correctly"""
+
+        result = self.client.get('/')
+
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'Hello', result.data)
+
+    def test_add_user(self):
+        """Tests the add user form is rendered"""
+
+        result = self.client.get('/adduser')
+
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'Register as a user', result.data)
+
+
