3570c579d7ff5af065a1c7f6427d423b71f50d78
StaceyCarter <stak1903@gmail.com>
2019-05-19 11:45:15 -0700

    query adjusted to order drugs alphabetically

--- queries.py
+++ queries.py
@@ -4,7 +4,7 @@ from model import connect_to_db, db
 def get_list_of_drugs():
     """Returns a list of all the drug names"""
 
-    drugs = Drug.query.all()
+    drugs = Drug.query.order_by('generic_name').all()
 
     list_drugs = [drug.generic_name for drug in drugs]
 
